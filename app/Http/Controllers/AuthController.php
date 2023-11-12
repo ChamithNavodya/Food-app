86e551dda055f7724a79105b602cf32c48ef758e
Chamith Navodya <c7navodya@gmail.com>
2023-11-12 17:48:33 +0530

    changed AuthController

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -37,37 +37,6 @@ class AuthController extends Controller
         return response()->json(['message' => 'Successfully logged out']);
     }
 
-    // public function register(Request $request) {
-    //     $validator = Validator::make($request->all(), [
-    //         'name' => 'required|string|max:255',
-    //         'email' => 'required|string|email|max:255|unique:users',
-    //         'password' => 'required|string|min:6',
-    //     ]);
-
-    //     if ($validator->fails()) {
-    //         return response()->json(['error' => $validator->errors()], 422);
-    //     }
-
-    //     $user = User::create([
-    //         'name' => $request->name,
-    //         'email' => $request->email,
-    //         'password' => bcrypt($request->password),
-    //     ]);
-
-    //     $token = Auth::login($user);
-
-    //     return response()->json([
-    //         'status' => 'success',
-    //         'message' => 'User created successfully',
-    //         'user' => $user,
-    //         'authorisation' => [
-    //             'token' => $token,
-    //             'type' => 'bearer',
-    //         ]
-    //     ]);
-
-    // }
-
 
     public function register(Request $request)
     {
@@ -84,8 +53,8 @@ class AuthController extends Controller
             return response()->json(['error' => $validator->errors()], 422);
         }
 
-        $input['password'] = bcrypt($input['password']); // use bcrypt to hash the passwords
-        $user = User::create($input); // eloquent creation of data
+        $input['password'] = bcrypt($input['password']);
+        $user = User::create($input);
 
         $success['user'] = $user;
 
@@ -99,12 +68,12 @@ class AuthController extends Controller
         try {
             $user = JWTAuth::parseToken()->authenticate();
             if (!$user) {
-                return $this->sendError([], "user not found", 403);
+                return response()->json(['error' => 'User not found'], 403);
             }
         } catch (JWTException $e) {
-            return $this->sendError([], $e->getMessage(), 500);
+            return response()->json(['error' => $e->getMessage()], 500);
         }
 
-        return $this->sendResponse($user, "user data retrieved", 200);
+        return response()->json(['data' => $user, 'message' => 'User data retrieved'], 200);
     }
 }
