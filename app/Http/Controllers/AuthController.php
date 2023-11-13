6543bee4eb6f4b99e89082203c60b7ddf6d53f38
Chamith Navodya <c7navodya@gmail.com>
2023-11-13 14:53:08 +0530

    AuthConrtoller Added

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Database\QueryException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
@@ -20,47 +21,82 @@ class AuthController extends Controller
         ]);
     }
 
-    public function login()
+    public function login(Request $request)
     {
-        $credentials = request(['email', 'password']);
+        try {
+            $request->validate([
+                'email' => 'required|email',
+                'password' => 'required|string',
+            ]);
+            $credentials = $request->only('email', 'password');
 
-        if (!$token = auth()->attempt($credentials)) {
-            return response()->json(['error' => 'Invalid credentials'], 401);
+            if (!$token = auth()->attempt($credentials)) {
+                return response()->json(['error' => 'Invalid credentials'], 401);
+            }
+
+            return $this->respondWithToken($token);
+        } catch (QueryException $exception) {
+
+            return response()->json(['error' => 'Database error: ' . $exception->getMessage()], 500);
+        } catch (\Exception $exception) {
+
+            return response()->json(['error' => 'An unexpected error occurred'], 500);
         }
-
-        return $this->respondWithToken($token);
     }
 
     public function logout()
     {
-        auth()->logout();
-        return response()->json(['message' => 'Successfully logged out']);
+        try {
+            $user = Auth::user();
+
+            if ($user) {
+                $user->tokens->each(function ($token) {
+                    $token->delete();
+                });
+            }
+
+            Auth::logout();
+
+            return response()->json(['message' => 'Successfully logged out']);
+        } catch (QueryException $exception) {
+
+            return response()->json(['error' => 'Database error: ' . $exception->getMessage()], 500);
+        } catch (\Exception $exception) {
+
+            return response()->json(['error' => 'An unexpected error occurred'], 500);
+        }
     }
 
 
     public function register(Request $request)
     {
-        $input = $request->only('name', 'email', 'password', 'c_password');
+        try {
+            $input = $request->only('name', 'email', 'password', 'c_password');
 
-        $validator = Validator::make($input, [
-            'name' => 'required',
-            'email' => 'required|email|unique:users',
-            'password' => 'required|min:8',
-            'c_password' => 'required|same:password',
-        ]);
+            $validator = Validator::make($input, [
+                'name' => 'required',
+                'email' => 'required|email|unique:users',
+                'password' => 'required|min:8',
+                'c_password' => 'required|same:password',
+            ]);
 
-        if ($validator->fails()) {
-            return response()->json(['error' => $validator->errors()], 422);
+            if ($validator->fails()) {
+                return response()->json(['error' => $validator->errors()], 422);
+            }
+
+            $input['password'] = bcrypt($input['password']);
+
+            $user = User::create($input);
+            $success['user'] = $user;
+
+            return response()->json([$success, 'user registered successfully'], 201);
+        } catch (QueryException $exception) {
+
+            return response()->json(['error' => 'Database error: ' . $exception->getMessage()], 500);
+        } catch (\Exception $exception) {
+
+            return response()->json(['error' => 'An unexpected error occurred'], 500);
         }
-
-        $input['password'] = bcrypt($input['password']);
-        $user = User::create($input);
-
-        $success['user'] = $user;
-
-        return response()->json([$success, 'user registered successfully'], 201);
-
-        // return $this->sendResponse($success, 'user registered successfully', 201);
     }
 
     public function getUser()
